3574ee7598ef37d2c0df99f2f12d514d51d3f6a9
Wachhund Skytower <Wachhund@users.noreply.github.com>
2026-02-17 15:10:22 +0100

    chore(release): update repo links and require PHP 8.5 for v1.1.0

--- app/views/layouts/_news.php
+++ app/views/layouts/_news.php
@@ -1,7 +1,7 @@
 <?php if (!CONFIG()->enable_news_ticker) return; ?>
 <div id="news-ticker" style="display: none">
   <ul>
-    <li>MyImouto is now on <a href="https://github.com/myimouto/myimouto">GitHub</a>.</li>
+    <li>MyImouto is now on <a href="https://github.com/Wachhund/myimouto">GitHub</a>.</li>
   </ul>
 
   <a href="#" id="close-news-ticker-link"><?= $this->t('.close') ?></a>
